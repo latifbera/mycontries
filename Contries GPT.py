88e71207ccb53d39d2cef3372e35d8eaa6910bf6
Latif Bera <107883542+latifbera@users.noreply.github.com>
2023-01-23 11:13:56 +0300

    Add files via upload
    Dictionary

---
+++ Contries GPT.py
@@ -0,0 +1,38 @@
+countries = {
+    "a": "Arnavutluk",
+    "b": "Bosna Hersek",
+    "c": "Cezayir",
+    "d": "Dominik Cumhuriyeti",
+    "e": "Etiyopya",
+    "f": "Fas",
+    "g": "Güney Afrika Cumhuriyeti",
+    "h": "Hırvatistan",
+    "i": "İzlanda",
+    "j": "Japonya",
+    "k": "Kamerun",
+    "l": "Letonya",
+    "m": "Mısır",
+    "n": "Nijerya",
+    "o": "Orta Afrika Cumhuriyeti",
+    "p": "Portekiz",
+    "q": "Quatar",
+    "r": "Rusya Federasyonu",
+    "s": "Sırbistan",
+    "t": "Tunus",
+    "u": "Ukrayna",
+    "v": "Venezuela",
+    "w": "Wales",
+    "x": "Xinjiang",
+    "y": "Yeni Zelanda",
+    "z": "Zimbabve",
+    "ç": "Çekoslovakya",
+    "ö": "Özbekistan",
+    "ş": "Şili",
+    "ü": "Ürdün",
+    "ı": "Irak",
+}
+
+x = input()
+
+print(countries.get(x, "Invalid input"))
+
