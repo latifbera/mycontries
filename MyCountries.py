b4a76ca47e0141273939a4cfbc9f22024952e651
Latif Bera <107883542+latifbera@users.noreply.github.com>
2023-01-23 11:10:07 +0300

    Add files via upload

---
+++ MyCountries.py
@@ -0,0 +1,63 @@
+x = input("Bir harf giriniz:  ")
+if x == "a":
+  print("Arnavutluk")
+if x == "b":
+    print("Bosna Hersek")
+if x == "c":
+      print("Cezayir")
+if x == "d":
+        print("Dominik Cumhuriyeti")
+if x == "e":
+        print("Etiyopya")
+if x == "f":
+        print("Fas")
+if x == "g":
+        print("Güney Afrika Cumhuriyeti")            
+if x == "h":
+        print("Hırvatistan")
+if x == "i":
+        print("İzlanda")
+if x == "j":
+        print("Japonya")
+if x == "k":
+        print("Kamerun")         
+if x == "l":
+        print("Letonya")
+if x == "m":
+        print("Mısır")
+if x == "n":
+        print("Nijerya")
+if x == "o":
+        print("Orta Afrika Cumhuriyeti")  
+if x == "p":
+        print("Portekiz")
+if x == "q":
+        print("Quatar")
+if x == "r":
+        print("Rusya Federasyonu")
+if x == "s":
+        print("Sırbistan")
+if x == "t":
+        print("Tunus")
+if x == "u":
+        print("Ukrayna")
+if x == "v":
+        print("Venezuela")
+if x == "w":
+        print("Wales")
+if x == "x":
+        print("Xinjiang")
+if x == "y":
+        print("Yeni Zelanda")
+if x == "z":
+        print("Zimbabve")
+if x == "ç":
+        print("Çekoslovakya")
+if x == "ö":
+        print("Özbekistan")
+if x == "ş":
+        print("Şili")
+if x == "ü":
+        print("Ürdün")
+if x == "ı":
+        print("Irak")                                      
